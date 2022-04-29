43b78381617342e2ab9ae69f6ed6117069d2f031
N1CK PL4Y <N1CK PL4Y@192.168.1.105>
2022-04-28 20:17:47 -0400

    se agregan los links de materialize

--- index.php
+++ index.php
@@ -6,11 +6,13 @@ Click nbfs://nbhost/SystemFileSystem/Templates/Project/PHP/PHPProject.php to edi
 <html>
     <head>
         <meta charset="UTF-8">
+        <link rel="stylesheet" href="Materialize/css/materialize.css">
+        <link rel="stylesheet" href="Materialize/css/materialize.min.css">
+        <link rel="stylesheet" href="Materialize/js/materialize.js"> 
+        <link rel="stylesheet" href="Materialize/js/materialize.min.js">
         <title></title>
     </head>
     <body>
-        <?php
-        // put your code here
-        ?>
+        
     </body>
 </html>
