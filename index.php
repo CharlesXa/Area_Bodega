ce0b137b729f0154cc8d051456405864d1728cec
N1CK PL4Y <N1CK PL4Y@192.168.1.105>
2022-04-28 21:40:57 -0400

    estructura login

--- index.php
+++ index.php
@@ -7,12 +7,44 @@ Click nbfs://nbhost/SystemFileSystem/Templates/Project/PHP/PHPProject.php to edi
     <head>
         <meta charset="UTF-8">
         <link rel="stylesheet" href="Materialize/css/materialize.css">
-        <link rel="stylesheet" href="Materialize/css/materialize.min.css">
-        <link rel="stylesheet" href="Materialize/js/materialize.js"> 
-        <link rel="stylesheet" href="Materialize/js/materialize.min.js">
-        <title></title>
+        <script src="Materialize/js/materialize.js"></script>
+        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+        <title>LogIn</title>
     </head>
     <body>
-        
+        <div class="container">
+            <div class="row" style="margin-top: 15%">
+                <div class="col s6 offset-s3">
+                    <div class="row">
+                        <div class="col s12">
+                            <form action="action">
+                                <div class="card" style="background-color: whitesmoke">
+                                    <div class="card-content">
+                                        <span class="card-title" align="center">Iniciar Sesion</span>
+                                        <div class="row">
+                                            <div class="input-field col s12">
+                                                <i class="material-icons prefix">account_circle</i>
+                                                <input id="txt_user" type="text">
+                                                <label for="txt_user">Nombre usuario</label>
+                                            </div>
+                                            <div class="input-field col s12">
+                                                <i class="material-icons prefix">lock_open</i>
+                                                <input id="txt_pass" type="password">
+                                                <label for="txt_pass">Password</label>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="card-action">
+                                        <div class="row">
+                                            <input class="waves-effect waves-light btn col s12" style="background-color: #009688" type="submit" value="Ingresar">
+                                        </div>      
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </body>
 </html>
