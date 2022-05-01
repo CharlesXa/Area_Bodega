a58b569ddf7b96ccdde3d0ab2d1960414aae899d
N1CK PL4Y <N1CK PL4Y@192.168.1.105>
2022-05-01 18:50:25 -0400

    estructura y validacion del login actualizado

--- index.php
+++ index.php
@@ -17,24 +17,24 @@ Click nbfs://nbhost/SystemFileSystem/Templates/Project/PHP/PHPProject.php to edi
                 <div class="col s6 offset-s3">
                     <div class="row">
                         <div class="col s12">
-                            <form action="action">
-                                <div class="card" style="background-color: whitesmoke">
+                            <form method="post" action="Controller/controller_login.php">
+                                <div class="card" style="background-color: whitesmoke; border-radius: 10px">
                                     <div class="card-content">
                                         <span class="card-title" align="center">Iniciar Sesion</span>
                                         <div class="row">
                                             <div class="input-field col s12">
                                                 <i class="material-icons prefix">account_circle</i>
-                                                <input id="txt_user" type="text">
-                                                <label for="txt_user">Nombre usuario</label>
+                                                <input id="rut" name="txt_rut" type="text" class="validate" required>
+                                                <label for="rut">R.U.T</label>
                                             </div>
                                             <div class="input-field col s12">
                                                 <i class="material-icons prefix">lock_open</i>
-                                                <input id="txt_pass" type="password">
-                                                <label for="txt_pass">Password</label>
+                                                <input id="passwd" name="txt_pass" type="password" class="validate" required>
+                                                <label for="passwd">Password</label>
                                             </div>
                                         </div>
                                     </div>
-                                    <div class="card-action">
+                                    <div class="card-action" style="border-radius: 10px">
                                         <div class="row">
                                             <input class="waves-effect waves-light btn col s12" style="background-color: #009688" type="submit" value="Ingresar">
                                         </div>      
