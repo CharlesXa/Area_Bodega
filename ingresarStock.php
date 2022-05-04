d5f0c82443c51b9c48d8e95affc096ad4ad4b548
jr972 <jr972@Charles_Xavier>
2022-05-03 22:50:37 -0400

    Archivos nuevos

--- ingresarStock.php
+++ ingresarStock.php
@@ -6,9 +6,15 @@ Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/EmptyPHPWebPage.php to 
 <html>
     <head>
         <meta charset="UTF-8">
-        <title></title>
+        <link rel="stylesheet" href="Materialize/css/materialize.css">
+        <script src="Materialize/js/materialize.js"></script>
+        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+        <script src="js/jquery.rut.js"></script>
+        <title>LogIn</title>
     </head>
     <body>
+        
         <?php
         // put your code here
         ?>
